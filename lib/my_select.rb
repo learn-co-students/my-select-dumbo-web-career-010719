ad0934890a32ee9b9e735fb42ada7a8b742e1567
mazenswar <mazenswar@protonmail.com>
2018-12-02 18:35:23 -0500

    my select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+ new_coll = []
+ if block_given?
+   i = 0
+   while i < collection.length
+     if yield(collection[i]) == true
+       new_coll << collection[i]
+     end
+     i += 1
+   end
+ else
+   collection
+ end
+ new_coll
 end
