20be3491d88baa4dd029846d6da386d8cc779f0f
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-13 23:36:11 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+
+def my_select(arr)
+  i = 0
+  newArr = []
+  while (i < arr.length)
+    if (yield(arr[i]))
+      newArr.push(arr[i])
+    end
+    i += 1
+  end
+  return newArr
 end
