4d81a1757fa35a05469f67a96a29c8b2b82ff63f
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-14 20:20:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    output = []
+    counter = 0
+    
+    while counter < collection.length
+      if yield collection[counter]
+        output.push(collection[counter])
+      end
+        
+      counter += 1
+    end
+    output
+  end
 end
