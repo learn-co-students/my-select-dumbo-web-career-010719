56f9383f3cb259c050ab3248d9e2ca9fd696649f
Jocake <joseph8p@gmail.com>
2018-12-17 06:08:35 -0800

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  count = 0
+  newColl = []
+  while collection.length > count
+    if (collection[count] % 2 == 0)
+      result = yield collection[count] # yield first
+      newColl.push(collection[count]) if result # then push to newColl if required
+    end
+    count += 1
+  end
+  newColl
 end
